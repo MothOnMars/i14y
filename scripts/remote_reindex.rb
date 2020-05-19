4b30abb937d943dd240b833b9e85bb5a18792d94
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2020-05-19 15:12:36 -0700

    puts instead of log

--- scripts/remote_reindex.rb
+++ scripts/remote_reindex.rb
@@ -16,15 +16,15 @@ def reindex_task(task_id)
 end
 
 def handle_failures(task, index)
-  Rails.logger.info task
+  puts task
   failures = task['error']
   return unless failures&.present?
 
-  Rails.logger.error("Error migrating #{index}:\n#{failures}")
+  puts("Error migrating #{index}:\n#{failures}")
 end
 
 def create_new_index(index_name)
-  Rails.logger.info "Creating index #{index_name}"
+  puts "Creating index #{index_name}"
   @reindexing_client.indices.create(index: index_name,
                                     body: { settings: { refresh_interval: '-1',
                                                         number_of_replicas: 0,
@@ -37,21 +37,22 @@ def process_alias(es_alias)
   alias_name = alias_names['aliases'].keys.first
   create_new_index(old_es_index_name)
   remote_reindex(old_es_index_name)
-  Rails.logger.info "adding alias #{alias_name} for #{old_es_index_name}"
+  puts "adding alias #{alias_name} for #{old_es_index_name}"
   @reindexing_client.indices.put_alias(index: old_es_index_name, name: alias_name)
 rescue => error
-  Rails.logger.error "Error processing #{es_alias}\n#{error}\n#{error.backtrace.first(2)}"
+  puts "Error processing #{es_alias}\n#{error}\n#{error.backtrace.first(2)}"
 end
 
 def remote_reindex(index)
-  Rails.logger.info("Remote reindexing #{index}")
+  puts("Remote reindexing #{index}")
   task_id = (@reindexing_client.reindex(body: reindex_body(index),
                                  wait_for_completion: false))['task']
+  #TODO: report progress
   while reindex_task(task_id)['completed'] == false
-    Rails.logger.info("Reindex in progress: #{index}")
+    puts("Reindex in progress: #{index}: #{task_id}")
     sleep 10
   end
-  Rails.logger.info("Reindex complete: #{index}")
+  puts("Reindex complete: #{index}")
   handle_failures(reindex_task(task_id), index)
 end
 
@@ -88,14 +89,14 @@ entity_names.each do |entity_name|
   persistence_model_klass = entity_name.singularize.camelize.constantize # Document < Object
   klass = entity_name.camelize.constantize # Documents < Object
   template_generator = klass.new #Documents.new
-  Rails.logger.info "Adding template for #{entity_name}"
+  puts "Adding template for #{entity_name}"
   @reindexing_client.indices.put_template(name: entity_name,
                                           body: template_generator.body,
                                           order: 0)
 
   wildcard = [persistence_model_klass.index_namespace, '*'].join # "development-i14y-documents*"
   aliases = Elasticsearch::Persistence.client.indices.get_alias(name: wildcard)
-  Rails.logger.info "found #{aliases.count} aliases for #{entity_name}"
+  puts "found #{aliases.count} aliases for #{entity_name}"
   aliases.each do |es_alias|
     process_alias(es_alias)
   end
