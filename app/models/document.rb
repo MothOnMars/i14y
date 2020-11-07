f94c1a36362ac80cf5a5e1e654d7fb075408ccd2
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2020-11-07 14:05:34 -0500

    move serialization code

--- app/models/document.rb
+++ app/models/document.rb
@@ -7,7 +7,6 @@ class Document
   extend ActiveModel::Callbacks
  # include Elasticsearch::Persistence::Model
   include Virtus.model
-  extend NamespacedIndex
  # include ActiveModel::Model
  #   include ActiveModel::Model
 
@@ -32,27 +31,5 @@ class Document
   before_save { self.changed = changed.presence || created }
 
   LANGUAGE_FIELDS = [:title, :description, :content]
-
-  gateway do
-    def serialize(document)
-      Serde.serialize_hash(document.to_hash, document.language, LANGUAGE_FIELDS)
-    end
-
-    def deserialize(hash)
-      doc_hash = hash['_source']
-      deserialized_hash = Serde.deserialize_hash(doc_hash, doc_hash['language'], LANGUAGE_FIELDS)
-
-      document = Document.new deserialized_hash
-      document.instance_variable_set :@_id, hash['_id']
-      document.instance_variable_set :@_index, hash['_index']
-      document.instance_variable_set :@_type, hash['_type']
-      document.instance_variable_set :@_version, hash['_version']
-
-      document.instance_variable_set :@hit, Hashie::Mash.new(hash.except('_index', '_type', '_id', '_version', '_source'))
-
-      document.instance_variable_set(:@persisted, true)
-      document
-    end
-  end
 
 end
--- app/repositories/document_repository.rb
+++ app/repositories/document_repository.rb
@@ -10,6 +10,25 @@ class DocumentRepository
 
   settings index: { number_of_shards: 1 }
 
+    def serialize(document)
+      Serde.serialize_hash(document.to_hash, document.language, LANGUAGE_FIELDS)
+    end
+
+    def deserialize(hash)
+      doc_hash = hash['_source']
+      deserialized_hash = Serde.deserialize_hash(doc_hash, doc_hash['language'], Document::LANGUAGE_FIELDS)
+
+      document = Document.new deserialized_hash
+      document.instance_variable_set :@_id, hash['_id']
+      document.instance_variable_set :@_index, hash['_index']
+      document.instance_variable_set :@_type, hash['_type']
+      document.instance_variable_set :@_version, hash['_version']
+
+      document.instance_variable_set :@hit, Hashie::Mash.new(hash.except('_index', '_type', '_id', '_version', '_source'))
+
+      document.instance_variable_set(:@persisted, true)
+      document
+    end
 
   #document_type '_doc' #needed?
 end
