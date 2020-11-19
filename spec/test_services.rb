44b836ee84fd50392d68e560702c5c711eea1b83
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2020-11-19 14:32:55 -0500

    clean up test services

--- spec/test_services.rb
+++ spec/test_services.rb
@@ -14,14 +14,12 @@ module TestServices
 
   # TODO: rename - only collections
   def create_es_indexes
-    collections_index_name = [Rails.env, I14y::APP_NAME, 'collections', 'v1'].join('-')
-    collection_repository = CollectionRepository.new(index_name: collections_index_name)
-    collection_repository.create_index!
-    #FIXME - do we even need the alias?
-    DEFAULT_CLIENT.indices.put_alias index: collections_index_name, name: collections_index_name.remove('-v1')
-    # alias is just the namespaced index wtihout the v1
-    #client.indices.put_alias index: es_collections_index_name, name: Collection.alias 
-   # Elasticsearch::Persistence.client.indices.put_alias index: es_collections_index_name, name: Collection.index_name
+    index_name = [Rails.env, I14y::APP_NAME, 'collections', 'v1'].join('-')
+    DEFAULT_CLIENT.indices.create(index: index_name) unless DEFAULT_CLIENT.indices.exists?(index: index_name)
+    DEFAULT_CLIENT.indices.put_alias(
+      index: index_name,
+      name: index_name.remove('-v1')
+    )
   end
 
   def create_documents_index(handle)
@@ -31,7 +29,7 @@ module TestServices
     #FIXME rescue nil - only create index once
     #TODO: ensure the ag blogs index isn't left behind after collections api spec
     DEFAULT_CLIENT.indices.delete(index: index_name) if DEFAULT_CLIENT.indices.exists?(index: index_name)
-    DEFAULT_CLIENT.indices.create(index: index_name, body: { settings: { number_of_shards: 1 } }) unless DEFAULT_CLIENT.indices.exists?(index: "test-i14y-documents-#{handle}-v1")
+    DEFAULT_CLIENT.indices.create(index: index_name, body: { settings: { number_of_shards: 1 } }) unless DEFAULT_CLIENT.indices.exists?(index: index_name)
     DEFAULT_CLIENT.indices.put_alias(
       index: index_name,
       name: index_name.remove('-v1')
@@ -39,30 +37,6 @@ module TestServices
   end
 
   def delete_es_indexes
-    #Elasticsearch::Persistence.client.indices.delete(index: [Rails.env, I14y::APP_NAME, '*'].join('-'))
     DEFAULT_CLIENT.indices.delete(index: [Rails.env, I14y::APP_NAME, '*'].join('-'))
   end
 end
-
-=begin
-  let(:document_repository) do
-    #FIXME
-    es_documents_index_name = [DocumentRepository.index_namespace('agency_blogs'), 'v1'].join('-')
-    #Using a single shard prevents intermittent relevancy issues in tests
-    #https://www.elastic.co/guide/en/elasticsearch/guide/current/relevance-is-broken.html
-    DocumentRepository.new(settings: { index: { number_of_shards: 1 } }, index_name: es_documents_index_name)
-  end
-
-
-  before do
-    es_documents_index_name = [DocumentRepository.index_namespace('agency_blogs'), 'v1'].join('-')
-    #FIXME: delete by query
-    #Using a single shard prevents intermittent relevancy issues in tests
-    #https://www.elastic.co/guide/en/elasticsearch/guide/current/relevance-is-broken.html
-    #DocumentRepository.settings(index: { number_of_shards: 1 })
-    document_repository.create_index!#(index_name: es_documents_index_name)
-    client.indices.put_alias index: es_documents_index_name,
-                                                        name: DocumentRepository.index_namespace('agency_blogs')
-    #Document.index_name = Document.index_namespace('agency_blogs')
-  end
-=end
