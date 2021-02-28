35f120decee0f7f83685c4d6979ab73f199cfd1e
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2021-02-28 11:27:00 -0500

    require collection

--- app/classes/collection_stats.rb
+++ app/classes/collection_stats.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class CollectionStats
-  
+  def initialize(collection)
+    
+  end
 end
--- spec/classes/collection_stats_spec.rb
+++ spec/classes/collection_stats_spec.rb
@@ -3,4 +3,9 @@
 require 'rails_helper'
 
 describe CollectionStats do
+  describe 'initialization' do
+    it 'requires a collection' do
+      expect { CollectionStats.new }.to raise_error ArgumentError
+    end
+  end
 end
