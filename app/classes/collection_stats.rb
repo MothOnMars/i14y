d9f00a1da039872df3d58e4989acd1f213559508
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2021-02-28 11:34:40 -0500

    document total

--- app/classes/collection_stats.rb
+++ app/classes/collection_stats.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class CollectionStats
+  attr_reader :collection
+
   def initialize(collection)
-    
+    @collection = collection
+  end
+
+  def document_total
+    0
   end
 end
--- spec/classes/collection_stats_spec.rb
+++ spec/classes/collection_stats_spec.rb
@@ -3,9 +3,20 @@
 require 'rails_helper'
 
 describe CollectionStats do
+  let(:collection) { Collection.new(id: 'agency_blogs', token: 'token') }
+  let(:collection_stats) { CollectionStats.new(collection) }
+ 
   describe 'initialization' do
     it 'requires a collection' do
       expect { CollectionStats.new }.to raise_error ArgumentError
+    end
+  end
+
+  describe '#document_total' do
+    subject(:document_total) { collection_stats.document_total }
+
+    describe 'by default' do
+      it { is_expected.to eq 0 }
     end
   end
 end
