e57d297fe1513cbb0cf4f2d76f629fc31c5c3f8c
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2021-02-28 13:24:32 -0500

    wip

--- app/classes/collection_stats.rb
+++ app/classes/collection_stats.rb
@@ -14,7 +14,11 @@ class CollectionStats
   end
 
   def last_document_sent
-    
+    document_repository =     DocumentRepository.new(
+      index_name: DocumentRepository.index_namespace(collection.id)
+    )
+    document_repository.search(size:1, sort: { updated_at: "desc"}).
+      results.first.updated_at.utc.to_s
   end
 end
 
@@ -37,4 +41,35 @@ end
       index_name: DocumentRepository.index_namespace(id)
     )
   end
+
+  index: documents_index_name,
+spec/requests/api/v1/documents_spec.rb-          id: id,
+spec/requests/api/v1/documents_spec.rb-          body: {
+spec/requests/api/v1/documents_spec.rb-            doc: {
+spec/requests/api/v1/documents_spec.rb-              updated_at: 1.year.ago,
+spec/requests/api/v1/documents_spec.rb-              created_at: 1.year.ago
+Binary file vendor/stream2es matches
+
+    client.bulk body: [
+      { index: { _index: 'myindex', _type: 'mytype', _id: 1 } },
+      { title: 'foo' },
+
+      { index: { _index: 'myindex', _type: 'mytype', _id: 2 } },
+      { title: 'foo' },
+
+      { delete: { _index: 'myindex', _type: 'mytype', _id: 3  } }
+    ]
+
+
+      describe '#last_document_sent' do
+    subject(:last_document_sent) { collection.last_document_sent }
+
+    context 'when something goes wrong' do
+      before do
+        allow_any_instance_of(DocumentRepository).
+          to receive(:search).and_raise(StandardError)
+      end
+
+      it { is_expected.to be nil }
+    end
 =end
--- spec/classes/collection_stats_spec.rb
+++ spec/classes/collection_stats_spec.rb
@@ -11,12 +11,14 @@ describe CollectionStats do
   shared_context 'when documents are associated with the collection' do
     let(:document1_params) do
       {
+        id: 1,
         path: 'https://agency.gov/1.html',
         language: 'en'
       }
     end
     let(:document2_params) do
       {
+        id: 2,
         path: 'https://agency.gov/1.html',
         language: 'en'
       }
@@ -61,6 +63,35 @@ describe CollectionStats do
 
     describe 'by default' do
       it { is_expected.to be nil }
+    end
+
+    context 'when documents are associated with the collection' do
+      include_context 'when documents are associated with the collection'
+
+      context 'when the documents were updated at different times' do
+        before do
+          ES.client.bulk(
+            body: [
+              { update: { _index: index_name, _type: '_doc', _id: 1, data: { doc: { updated_at: DateTime.new(2020, 1, 1).utc } } } },
+              { update: { _index: index_name, _type: '_doc', _id: 2, data: { doc: { updated_at: DateTime.new(2021, 1, 1).utc } } } }
+            ],
+            refresh: true
+          )
+        end
+
+        it 'returns the strigified timestamp of the most recently updated document' do
+          expect(last_document_sent).to eq('2021-01-01 00:00:00 UTC')
+        end
+      end
+
+      context 'when something goes wrong' do
+        before do
+          allow_any_instance_of(DocumentRepository).
+            to receive(:search).and_raise(StandardError)
+        end
+
+        xit { is_expected.to be nil }
+      end
     end
   end
 end
